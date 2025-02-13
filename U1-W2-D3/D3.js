868e067f2cd70fa480e73fb0a2645768ffe6e7f1
Joselinedesouza <joselinedesouza022@gmail.com>
2025-02-13 20:04:26 +0100

    Update.D3js

--- U1-W2-D3/D3.js
+++ U1-W2-D3/D3.js
@@ -189,10 +189,33 @@ console.log("massa totale dell'equipaggio: ", crewMass);
 
   Una volta fatto, modifica la massa di qualche elemento dell'equipaggio e vedi se riesci ad ottenere un messaggio diverso.
 */
-
+switch (true) {
+  case crewMass < 500:
+    console.log("Ship is under loaded");
+    break;
+  case crewMass > 500 && crewMass < 700:
+    console.log("Ship is half loaded");
+    break;
+  case crewMass > 700 && crewMass < 900:
+    console.log("Warning: Load is over 700");
+    break;
+  case crewMass > 900 && crewMass < 1000:
+    console.log("Critical Load: Over 900");
+    break;
+  case crewMass > 1000:
+    console.log("DANGER! OVERLOAD ALERT: escape from ship now!");
+    break;
+  default:
+    console.log("No data provided");
+}
 /* ESERCIZIO 8
   Usa un for loop per cambiare il valore della proprietà "gender" di alcuni personaggi dal valore "n/a" a "robot" (Tip: puoi effettuare la riassegnazione del valore corrispondente o creare un nuovo array)
 */
+for (let i = 0; i < starWarsCharacters.length; i++) {
+  if (starWarsCharacters[i].gender === "n/a") {
+    starWarsCharacters[i].gender = "robot";
+  }
+}
 
 /* --EXTRA-- ESERCIZIO 9
   Utilizzando gli elementi presenti nell'array "femaleCharacters" rimuovi dall'array "charactersNames" le stringhe corrispondenti a personaggi con lo stesso nome.
